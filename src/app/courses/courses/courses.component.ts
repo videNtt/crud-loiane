4fec6a7dfd5d71d938d13729a6b0c2c5acfce700
Rodrigo Almeida <rodrigoalmeida_ata@hotmail.com>
2022-02-27 22:33:30 -0300

    first commit

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses$: Observable<Course[]>;
+  displayedColumns = ['name', 'category'];
+
+  constructor(private coursesServices: CoursesService) {
+    this.courses$ = this.coursesServices.list();
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
